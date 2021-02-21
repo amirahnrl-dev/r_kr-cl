a1a601120c938a45737e0c3730c199a4bd2aa2a7
Amirah Nrl <seasheep@outlook.com>
2021-02-21 15:01:02 +0800

    done toggle signin, signup using useLocation

--- src/header/header.jsx
+++ src/header/header.jsx
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { Component } from 'react';
 import { Link } from 'react-router-dom';
 import { auth } from '../utils/firebase';
 import { connect } from 'react-redux';
@@ -6,28 +6,30 @@ import { useLocation } from 'react-router-dom';
 import './header.scss'
 import { ReactComponent as Logo } from '../assets/crown.svg';
 
-const Header = ({ currentUser }) => (
-    <div className="header">
-        <Link className="logo-container" to ="/">
-            <Logo className="logo" />
-        </Link>
-        <nav>
-            <Link className="link" to="/shop">SHOP</Link>
-            <Link className="link" to="/contact">CONTACT</Link>
-            {
-                currentUser ? 
-                <div className="link" onClick={ () => auth.signOut() }>
-                    SIGN OUT
-                </div> : 
-                <Link className="link" to="/signup">SIGN UP</Link>
-                /*
-                <Link className="link" to="/signup">SIGN UP</Link> :
-                <Link className="link" to="/signin">SIGN IN</Link>
-                */
-            }
-        </nav>
-    </div>
-);
+const Header = ({ currentUser }) => {
+    const location = useLocation();
+
+    return ( 
+        <div className="header">
+            <Link className="logo-container" to ="/">
+                <Logo className="logo" />
+            </Link>
+            <nav>
+                <Link className="link" to="/shop">SHOP</Link>
+                <Link className="link" to="/contact">CONTACT</Link>
+                {
+                    currentUser ? 
+                    <div className="link" onClick={ () => auth.signOut() }>
+                        SIGN OUT
+                    </div> : (location.pathname === "/signin") ?
+                    <Link className="link" to="/signup">SIGN UP</Link> :
+                    <Link className="link" to="/signin">SIGN IN</Link>
+                }
+            </nav>
+        </div>
+    );
+}
+
                         // state -> rootReducer
 const mapStateToProps = state => ({
     currentUser: state.user.currentUser
