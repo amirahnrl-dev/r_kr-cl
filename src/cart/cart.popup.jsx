95e53c8aa015e9af318f7f0d22902bb95787c7bc
Amirah Nrl <seasheep@outlook.com>
2021-02-22 08:10:46 +0800

    add cart.item

--- src/cart/cart.popup.jsx
+++ src/cart/cart.popup.jsx
@@ -1,12 +1,25 @@
 import React from 'react';
+import { connect } from 'react-redux';
 import './cart.popup.scss';
 import Button from '../utils/button/button';
+import CartItem from './cart.item';
 
-const CartPopup = () => (
+const CartPopup = ({ cartItems }) => (
     <div className="cart-popup">
-        <div className="cart-items" />
+        <div className="cart-items">
+            {
+                cartItems.map(cartItem => (
+                        <CartItem key={ cartItem.id } item={cartItem} />
+                    )
+                )
+            }
+        </div>
         <Button>CHECKOUT</Button>
     </div>
 );
 
-export default CartPopup;
+const mapStateToProps = ({ cart: { cartItems } }) => ({
+    cartItems
+});
+
+export default connect(mapStateToProps)(CartPopup);
