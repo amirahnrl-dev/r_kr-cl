200987799851448abc8a53a51583bee53b513917
Amirah Nrl <seasheep@outlook.com>
2021-02-22 11:08:20 +0800

    accumulate item in cart quantity for display on icon - WITHOUT selector

--- src/cart/cart.icon.jsx
+++ src/cart/cart.icon.jsx
@@ -4,15 +4,23 @@ import './cart.icon.scss';
 import { connect } from 'react-redux';
 import { toggleHideCartPopup } from '../utils/redux/cart/cart.actions';
 
-const CartIcon = ({ toggleHideCartPopup }) => (
+const CartIcon = ({ toggleHideCartPopup, itemCount }) => (
     <div className="cart-icon" onClick={ toggleHideCartPopup }>
         <Icon className="icon" />
-        <span className="item-count">0</span>
+        <span className="item-count">{ itemCount }</span>
     </div>
 );
+
+const mapStateToProps = ({ cart: { cartItems } }) => ({
+    itemCount: cartItems.reduce((acc, cartItem) => (
+            acc + cartItem.quantity
+        ), 
+        0   // starting from
+    )
+});
 
 const mapDispatchToProps = dispatch => ({
     toggleHideCartPopup: () => dispatch(toggleHideCartPopup())
 });
 
-export default connect(null, mapDispatchToProps)(CartIcon);
+export default connect(mapStateToProps, mapDispatchToProps)(CartIcon);
