269a6dbc478260d04c22a92dae72ae004e3089b9
Amirah Nrl <seasheep@outlook.com>
2021-02-25 16:56:33 +0800

    collection routing & selector

--- src/collection/collection.page.jsx
+++ src/collection/collection.page.jsx
@@ -1,11 +1,17 @@
 import React from 'react';
+import { conenct, connect } from 'react-redux';
 import './collection.page.scss';
 import CollectionItem from './collection.item';
+import { selectCollection } from '../utils/redux/shop/shop.selectors';
 
-const CollectionPage = ({ match }) => (
+const CollectionPage = ({ collection }) => (
     <div className="collection-page">
         <h2>COLLECTION PAGE</h2>
     </div>
 );
 
-export default CollectionPage;
+const mapStateToProps = (state, ownProps) => ({
+    collection: selectCollection(ownProps.match.params.collectionId)(state)
+});
+
+export default connect(mapStateToProps)(CollectionPage);
