b374bd09b8d2cd340e0e27a3ba3a91af36be1b73
Amirah Nrl <seasheep@outlook.com>
2021-02-26 08:04:31 +0800

    data normalization in shop.data, and update  shop.selector to support it. changed collection.item width, update collection.page

--- src/collection/collection.page.jsx
+++ src/collection/collection.page.jsx
@@ -1,14 +1,25 @@
 import React from 'react';
-import { conenct, connect } from 'react-redux';
+import { connect } from 'react-redux';
 import './collection.page.scss';
 import CollectionItem from './collection.item';
 import { selectCollection } from '../utils/redux/shop/shop.selectors';
 
-const CollectionPage = ({ collection }) => (
-    <div className="collection-page">
-        <h2>COLLECTION PAGE</h2>
-    </div>
-);
+const CollectionPage = ({ collection }) => {
+    const { title, items } = collection;
+    return (
+        <div className="collection-page">
+            <h2 className="title">{ title }</h2>
+            <div className="items">
+                {
+                    items.map(item => (
+                            <CollectionItem key={ item.id } item={ item } />
+                        )
+                    )
+                }
+            </div>
+        </div>
+    );
+}
 
 const mapStateToProps = (state, ownProps) => ({
     collection: selectCollection(ownProps.match.params.collectionId)(state)
